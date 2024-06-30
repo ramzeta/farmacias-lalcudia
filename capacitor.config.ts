9217d5def4c20b65401ebbdcbf75a9f405404b5c
ramzeta <ramiroperezsanz13@gmail.com>
2024-06-30 18:17:49 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'farmaciaApp',
+  webDir: 'www'
+};
+
+export default config;
